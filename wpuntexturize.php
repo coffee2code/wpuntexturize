8a9519cca0a58fcb7819de7a4819cb31790697b7
Scott Reilly <scott@coffee2code.com>
2008-01-28 09:07:45 +0000

    Update to v1.0
    * Modify core code to consolidate `str_replace()` calls into one
    * Change description
    * Add extended description and installation instructions
    * Change from BSD license

--- wpuntexturize.php
+++ wpuntexturize.php
@@ -1,48 +1,57 @@
 <?php
 /*
 Plugin Name: wpuntexturize
-Version: 0.91
-Plugin URI: http://www.coffee2code.com/wp-plugins/
+Version: 1.0
 Author: Scott Reilly
 Author URI: http://www.coffee2code.com
-Description: Undo certain wptexturize modifications.
+Description: Prevent WordPress from displaying single and double quotation marks as their curly alternatives.
+
+Despite the unfortunately misleading name, this plugin is NOT the antithesis of WordPress's wptexturize() function.
+This ONLY prevents WordPress from making HTML entity code substitutions of single and double quotation marks with 
+their curly alternatives and does NOT prevent wptexturize() from making any other character and string substitutions. 
+
+Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, and 2.3+.
+
+=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
+=>> for more information and the latest updates
+
+Installation:
+
+1. Download the file http://www.coffee2code.com/wp-plugins/wpuntexturize.zip and unzip it into your 
+/wp-content/plugins/ directory.
+-OR-
+Copy and paste the the code ( http://www.coffee2code.com/wp-plugins/wpuntexturize.phps ) into a file called 
+wpuntexturize.php, and put that file into your /wp-content/plugins/ directory.
+2. Activate the plugin through the 'Plugins' admin menu in WordPress
 
 */
 
 /*
-Copyright (c) 2004, Scott Reilly
-Released under the BSD License
-All rights reserved.
+Copyright (c) 2004-2008 by Scott Reilly (aka coffee2code)
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following 
-conditions are met:
-   
-     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
-disclaimer in the documentation and/or other materials provided with the distribution.
-     * Neither the name of coffee2code.com nor the names of its contributors may be used to endorse or promote products derived from 
-this software without specific prior written permission.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT 
-NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
-THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 function wpuntexturize($text) {
-	$text = str_replace('&#8216;', "'", $text);
-	$text = str_replace('&#8217;', "'", $text);
-	$text = str_replace('&#8220;', '"', $text);
-	$text = str_replace('&#8221;', '"', $text);
-	return $text;
+	$char_codes = array('&#8216;', '&#8217;', '&#8220;', '&#8221;');
+	$replacements = array("'", "'", '"', '"');
+	return str_replace($char_codes, $replacements, $text);
 } // end wpuntexturize()
 
-add_filter('comment_text', 'wpuntexturize',11);
-add_filter('single_post_title', 'wpuntexturize',11);
-add_filter('the_title', 'wpuntexturize',11);
-add_filter('the_content', 'wpuntexturize',11);
-add_filter('the_excerpt', 'wpuntexturize',11);
+add_filter('comment_text', 'wpuntexturize', 11);
+add_filter('single_post_title', 'wpuntexturize', 11);
+add_filter('the_title', 'wpuntexturize', 11);
+add_filter('the_content', 'wpuntexturize', 11);
+add_filter('the_excerpt', 'wpuntexturize', 11);
 
 ?>
