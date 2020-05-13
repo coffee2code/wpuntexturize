35fabe790a68eaa33768478d93b114eabc04f036
Scott Reilly <scott@coffee2code.com>
2020-05-13 00:32:54 -0700

    Update links to coffee2code.com to be HTTPS

--- wpuntexturize.php
+++ wpuntexturize.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name: wpuntexturize
  * Version:     1.7.1
- * Plugin URI:  http://coffee2code.com/wp-plugins/wpuntexturize/
+ * Plugin URI:  https://coffee2code.com/wp-plugins/wpuntexturize/
  * Author:      Scott Reilly
- * Author URI:  http://coffee2code.com/
+ * Author URI:  https//coffee2code.com/
  * Text Domain: wpuntexturize
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
